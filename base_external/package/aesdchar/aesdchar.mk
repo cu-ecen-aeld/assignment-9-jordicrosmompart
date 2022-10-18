f516d1046f8cf47d53293aba7d1e72c8164c6bec
Jordi <jordi.cros.mompart@gmail.com>
2022-10-18 22:01:24 +0000

    mk for the driver supposed to be correct.

--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -15,17 +15,4 @@ AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
 AESDCHAR_MODULE_SUBDIRS += aesd-char-driver
 
-define AESDCHAR_BUILD_CMDS
-	#Assignment 8 char driver
-	$(MAKE) -C $(@D)/aesd-char-driver
-endef
-
-#Add writer, finder and finder-test utilities/scripts to the installation steps below
-define AESDCHAR_INSTALL_TARGET_CMDS
-	#Assignment 8 char driver
-	$(INSTALL) -d 0755 $(TARGET_DIR)/lib/modules/5.15.18/extra/
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar.ko $(TARGET_DIR)/lib/modules/5.15.18/extra/aesdchar.ko
-
-endef
-
 $(eval $(generic-package))
