be2ea2355127f81cdf649e9d6c09cf47ed62651a
Jordi <jordi.cros.mompart@gmail.com>
2022-10-18 22:24:51 +0000

    ./sockettest.sh works with QEMU

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'd2a013b8f95e547643d5b6368839f803d52f599f'
+AESD_ASSIGNMENTS_VERSION = '5909ce3c29fdc0a65541118581b2ca76af49e7df'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,13 +6,13 @@
 ##############################################################
 $(eval $(kernel-module))
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = 'd2a013b8f95e547643d5b6368839f803d52f599f'
+AESDCHAR_VERSION = '5909ce3c29fdc0a65541118581b2ca76af49e7df'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
 AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jordicrosmompart.git'
 AESDCHAR_SITE_METHOD = git
 AESDCHAR_GIT_SUBMODULES = YES
-AESDCHAR_MODULE_SUBDIRS += aesd-char-driver
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
 
 $(eval $(generic-package))
