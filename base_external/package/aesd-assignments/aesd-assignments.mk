12689723ed3d1a6099da77f6d295970ea8964b8a
Jordi <jordi.cros.mompart@gmail.com>
2022-10-20 17:05:51 +0000

    New version of the driver and memory leak detector included

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '5909ce3c29fdc0a65541118581b2ca76af49e7df'
+AESD_ASSIGNMENTS_VERSION = 'eca58e880321190cc984f3cc20736f4a9f65e8e5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 $(eval $(kernel-module))
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = '5909ce3c29fdc0a65541118581b2ca76af49e7df'
+AESDCHAR_VERSION = 'eca58e880321190cc984f3cc20736f4a9f65e8e5'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
